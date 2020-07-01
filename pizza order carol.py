dc97f66b93ac07af52f09c0170eb54e7401b3690
carolchen943 <62219003+carolchen943@users.noreply.github.com>
2020-07-02 09:02:53 +1200

    Update pizza order carol.py
    version 2 get

--- pizza order carol.py
+++ pizza order carol.py
@@ -8,20 +8,41 @@ class Main:
         num_cols = 3
         num_rows = 2
         global images
-        regular_name = ['Margherita', 'Kiwi', 'Garlic', 'Cheese', 'Hawaiian', 'Mediterranean (vegan)']
-        regular_img = ['img/Margherita.gif', 'img/Kiwi.gif', 'img/Garlic.gif', 'img/Cheese.gif',
-                       'img/Hawaiian.gif', 'img/Mediterranean (vegan).gif']
-        #regular_
-        gourment_name = ['Meat', 'Chicken Cranberry', 'Satay Chicken', 'Big BBQ Bacon', 'Veggie', 'Meatlovers']
-        gourment_img = ['img/Meat.jpg', 'img/Chicken Cranberry.jpg', 'img/Satay Chicken.jpg', 'img/Big BBQ Bacon.jpg'
-   , 'img/Veggie.jpg', 'img/Meatlovers.jpg']
+        
+        regular_name = [
+            'Margherita', 'Kiwi', 'Garlic', 'Cheese', 'Hawaiian', 'Mediterranean (vegan)'
+        ]
+        regular_img = [
+            'img/Margherita.gif', 'img/Kiwi.gif', 'img/Garlic.gif', 'img/Cheese.gif',
+            'img/Hawaiian.gif', 'img/Mediterranean (vegan).gif'
+        ]
+        regular_content = [
+            'Fresh tomato, mozzarella, fresh basil, parmesan', 'Bacon, egg, mozzarella',
+            'Mozzarella, garlic', 'Mozzarella, oregano',
+            'Ham, pineapple, mozzarella', 'Lebanese herbs, olive oil, fresh tomatoes, olives, onion'
+        ]
+        gourment_name = [
+            'Meat', 'Chicken Cranberry', 'Satay Chicken', 'Big BBQ Bacon', 'Veggie', 'Meatlovers'
+        ]
+        gourment_img = [
+            'img/Margherita.gif', 'img/Kiwi.gif', 'img/Garlic.gif', 'img/Cheese.gif',
+            'img/Hawaiian.gif', 'img/Mediterranean (vegan).gif'
+        ]
+        gourment_content = [
+            'Bacon, pancetta, ham, onion, pepperoni, mozzarella', 'Smoked chicken, cranberry, camembert mozzarella',
+            'Smoked chic, onions, capsicum, pine nuts, satay sauce, mozzarella Chilli flakes and dried basil',
+            'Smoky Bacon served on our classic marinara tomato sauce, heaped with mozzarella, topped off with a sweet and tangy BBQ drizzle', 
+            'sweet red onion, mushroom, red capsicum & melting mozzarella with drizzles of our tangy roast capsicum drizzle, finished with a dash of oregano',
+            'Spicy pepperoni, Italian sausage, succulent ham, seasoned ground beef and crispy bacon all piled onto classic marinara sauce and finished with cheesy mozzarella and a drizzle of BBQ'
+        ]
 
-        #top menu frame
-        self.frame1 = Frame(parent, bg="linen", padx=30, width = WIDTH, height = 30)
+
+        #create the header frame
+        self.frame1 = Frame(parent, bg="linen", padx=30, height = 30)
         self.frame1.grid(row = 0, columnspan = 7)
         #welcome label
         welcome_label = Label(self.frame1, text = "Welcome, ", bg = "linen")
-        welcome_label.grid(row=0, column=0, sticky = W, pady = 1)
+        welcome_label.grid(row=0, column=0, sticky = W, padx = 3)
         #logout button
         logout_btn = Button(self.frame1, text = "Log out", bg = "linen")
         logout_btn.grid(row=0, column=1, sticky = W, pady = 1)
@@ -45,30 +66,46 @@ class Main:
         #regular frame        
         self.frame2 = Frame(parent, bg="linen", width = WIDTH, height = 500)
         self.frame2.grid(row = 1, columnspan = 4)
-        #regular product frame
-        self.product_frame = []
+        #product frame
+        self.re_product_frame = []
         for i in range(len(regular_name)):
-            self.product_frame.append(Frame(self.frame2, bg="linen", width = WIDTH, height = 50))
+            self.re_product_frame.append(Frame(self.frame2, bg="linen", width = WIDTH, height = 50))
         frame_count = 0
         for c in range(num_cols):
             for r in range(num_rows):
                 if frame_count < len(regular_name):
-                    self.product_frame[frame_count].grid(row = r, column = c)
+                    self.re_product_frame[frame_count].grid(row = r, column = c)
                     frame_count += 1
-        #names and images of each pizza
-        self.name_label = []
-        self.img_label = []
+        #names and images of each regular pizza
+        self.re_name_label = []
+        self.re_img_label = []
+        self.re_content_label = []
+        self.re_add_cart_btn = []
         i = 0
-        for item in regular_name:
-            print(i, len(self.product_frame), regular_name)
-            
-            self.name_label.append(Label(self.product_frame[i], text = item))
-            
-            self.name_label[i].grid(row = 0, column = 0, sticky = N+W+S+E, pady = 5)
 
-            self.img_label.append(Label(self.product_frame[i], image=PhotoImage(file = regular_img[i])))
-            self.img_label[i].grid(row = 1, column = 0)     
+        images = []
+        for r in regular_img:
+            images.append(PhotoImage(file = r))
+        # print(images)
+        for i in range(len(regular_name)):
+            #print(i, len(self.product_frame), regular_name,images[i])
+            self.re_name_label.append(Label(self.re_product_frame[i], text = regular_name[i]))
+            self.re_name_label[i].grid(row = 0, column = 0, sticky = N+W+S+E, pady = 5)
+
+            self.re_img_label.append(Label(self.re_product_frame[i], image=images[i]))
+            self.re_img_label[i].grid(row = 1, column = 0)
+
+            self.re_content_label.append(Label(self.re_product_frame[i], text = regular_content[i]))
+            self.re_content_label[i].grid(row = 2, column = 0, sticky = N+W+S+E, pady = 3)
+
+            self.re_add_cart_btn.append(Button(self.re_product_frame[i], text = "ADD TO CART", fg = "red"))
+            self.re_add_cart_btn[i].grid(row = 3, column = 0, sticky = N+W+S+E, pady = 1)
             i += 1
+    
+   
+          
+
+
 #main
 if __name__ == '__main__':
     root = Tk()
