0a6f0b1f2d17d69abdfdeeacc0949252f578de98
carolchen943 <62219003+carolchen943@users.noreply.github.com>
2020-07-03 09:07:00 +1200

    Update pizza order carol.py
    version6 get, make the add number buttons and reduce number buttons of both regular and gourmet page work. I a learn new thing to help me complete my intension.

--- pizza order carol.py
+++ pizza order carol.py
@@ -1,10 +1,10 @@
 from tkinter import *
-#from PIL import ImageTk, Image 
+
 class Main:
     def __init__(self, parent):
         
         self.WIDTH = 1000
-        self.HEIGHT = 800
+        self.HEIGHT = 700
         self.num_cols = 3
         self.num_rows = 2
         global images
@@ -12,9 +12,10 @@ class Main:
 
         images = []
         gou_images = []
-
+        self.ReNum = [0,0,0,0,0,0]
+        self.GouNum = [0,0,0,0,0,0] #create list of the number shown in the number counter, with the initial numbers 0.
         root.geometry('{}x{}'.format(self.WIDTH,self.HEIGHT))
-        # forbid user to resize the GUI window
+        #forbid user resize the GUI window
         root.resizable(0, 0)
         
         self.regular_name = [
@@ -29,14 +30,14 @@ class Main:
             'Mozzarella, garlic', 'Mozzarella, oregano',
             'Ham, pineapple, mozzarella', 'Lebanese herbs, olive oil, fresh tomatoes, olives, onion'
         ]
-        self.gourment_name = [
+        self.gourmet_name = [
             'Meat', 'Chicken Cranberry', 'Satay Chicken', 'Big BBQ Bacon', 'Veggie', 'Meatlovers'
         ]
-        self.gourment_img = [
-            'img/Margherita.gif', 'img/Kiwi.gif', 'img/Garlic.gif', 'img/Cheese.gif',
-            'img/Hawaiian.gif', 'img/Mediterranean (vegan).gif'
+        self.gourmet_img = [
+            'img/Meat.gif', 'img/Chicken Cranberry.gif', 'img/Satay Chicken.gif', 'img/Big BBQ Bacon.gif',
+            'img/Veggie.gif', 'img/Meatlovers.gif'
         ]
-        self.gourment_content = [
+        self.gourmet_content = [
             'Bacon, pancetta, ham, onion, pepperoni, mozzarella', 'Smoked chicken, cranberry, camembert mozzarella',
             'Smoked chic, onions, capsicum, pine nuts, satay sauce, mozzarella Chilli flakes and dried basil',
             'Smoky Bacon served on our classic marinara tomato sauce, heaped with mozzarella, topped off with a sweet and tangy BBQ drizzle', 
@@ -47,7 +48,7 @@ class Main:
        
 
         #create canvas
-        self.f = Frame(parent, relief=FLAT, bd=-2)
+        self.f = Frame(parent, relief=FLAT, bd=-2, bg='linen')
         self.f.place(x=0, y=80, width=self.WIDTH, height=self.HEIGHT-80)
 
         self.canvas = Canvas(self.f, bd=-2)
@@ -75,10 +76,10 @@ class Main:
         logout_btn = Button(self.frame1, text = "Log out", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "red")
         logout_btn.grid(row=0, column=1, sticky = W, padx = 12, pady = 15)
         #regular pizza menu button
-        regular_btn = Button(self.frame1, text = "Regular pizzas ", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "red", command = self.regular)
+        regular_btn = Button(self.frame1, text = "Regular pizzas ", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "coral", command = self.regular)
         regular_btn.grid(row=0, column=2, sticky = W, padx = 12, pady = 15)
         #gourmet pizza menu button
-        gourmet_btn = Button(self.frame1, text = "Gourmet pizzas", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "red", command = self.gourment)
+        gourmet_btn = Button(self.frame1, text = "Gourmet pizzas", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "coral", command = self.gourmet)
         gourmet_btn.grid(row=0, column=3, sticky = W, padx = 12, pady = 15)
         #check out button
         checkout_btn = Button(self.frame1, text = "Checkout", bg = "linen", font=("Conmic Sans MS","20","bold"), fg = "red")
@@ -91,7 +92,7 @@ class Main:
         total_label.grid(row=0, column=6, sticky = W, padx = 12, pady = 15)
         
         self.regular_frame = Frame(self.frame, bg="linen", width = self.WIDTH, height = self.HEIGHT)
-        self.gourment_frame = Frame(self.frame, bg="linen", width = self.WIDTH, height = self.HEIGHT)
+        self.gourmet_frame = Frame(self.frame, bg="linen", width = self.WIDTH, height = self.HEIGHT)
 
         #default get into regular page first
         self.regular()
@@ -107,12 +108,12 @@ class Main:
 
 
     def regular(self):
-        self.gourment_frame.grid_forget()
+        self.gourmet_frame.grid_forget()
 
         # self.canvas.create_window((0, 30),  window=self.regular_frame)    
         self.regular_frame.grid(row = 1, columnspan = 4) 
         
-        #每个商品的小frame
+        #product frame
         self.re_product_frame = []
         for i in range(len(self.regular_name)):
             self.re_product_frame.append(Frame(self.regular_frame, bg="linen", width = self.WIDTH, height = 60))
@@ -132,14 +133,14 @@ class Main:
         self.re_add_num_btn = []
         self.re_add_cart_btn = []
         i = 0
-
+        
         # images = []
         for r in self.regular_img:
             images.append(PhotoImage(file = r))
         # print(images)
         for i in range(len(self.regular_name)):
             #print(i, len(self.product_frame), regular_name,images[i])
-            self.re_name_label.append(Label(self.re_product_frame[i], text = self.regular_name[i], font=("Conmic Sans MS","20","bold"), fg = "grey"))
+            self.re_name_label.append(Label(self.re_product_frame[i], text = self.regular_name[i], font=("Conmic Sans MS","20","bold"), fg = "dimgray"))
             self.re_name_label[i].grid(row = 0, column = 0, sticky = N+W+S+E, pady = 5)
 
             self.re_img_label.append(Label(self.re_product_frame[i], image=images[i]))
@@ -148,16 +149,16 @@ class Main:
             self.re_content_label.append(Label(self.re_product_frame[i], text = self.regular_content[i]))
             self.re_content_label[i].grid(row = 2, column = 0, sticky = N+W+S+E, pady = 3)
 
-            self.re_price_label.append(Label(self.re_product_frame[i], text = "$10", font=("Conmic Sans MS","16","bold"), fg = "red"))
+            self.re_price_label.append(Label(self.re_product_frame[i], text = "$10", font=("Conmic Sans MS","16","bold"), fg = "red", bg="linen"))
             self.re_price_label[i].grid(row = 3, column = 0, sticky = W, padx = 50)
 
-            self.re_reduce_num_btn.append(Button(self.re_product_frame[i], text = "-"))
+            self.re_reduce_num_btn.append(Button(self.re_product_frame[i], text = "-", command =lambda x=i: self.re_ReduceNumber(x)))
             self.re_reduce_num_btn[i].grid(row = 3, column = 0, sticky = E, pady = 0, padx = 80)
 
-            self.re_num_label.append(Label(self.re_product_frame[i], text = "0"))
+            self.re_num_label.append(Label(self.re_product_frame[i], text = self.ReNum[i]))
             self.re_num_label[i].grid(row = 3, column = 0, sticky = E, pady = 0, padx = 50)
             
-            self.re_add_num_btn.append(Button(self.re_product_frame[i], text = "+"))
+            self.re_add_num_btn.append(Button(self.re_product_frame[i], text = "+", command =lambda x=i: self.re_AddNumber(x)))
             self.re_add_num_btn[i].grid(row = 3, column = 0, sticky = E, pady = 0, padx = 20)
             
             self.re_add_cart_btn.append(Button(self.re_product_frame[i], text = "ADD TO CART", fg = "red"))
@@ -165,16 +166,7 @@ class Main:
    
         # self.frame2.grid(row = 1, columnspan = 4)
         # self.frame2.grid_propagate(0)
-
-    def gourment(self):
-        #########gourment part###########
-        #create gourment frame
-        self.regular_frame.grid_forget()
-        ##self.gourment_frame.grid(row = 0, columnspan = 3)        
-        # self.canvas.create_window((0, 30), window=self.gourment_frame)
-        self.gourment_frame.grid(row = 1, columnspan = 3)
-        # self.regular_frame.grid_propagate(0)
-        #create gourment pizza product frames
+#create gourment pizza product frames
         gou_num_cols = 3
         gou_num_rows = 6
         self.gou_product_img_frame = []
@@ -187,10 +179,23 @@ class Main:
         self.gou_num_label = []
         self.gou_add_num_btn = []
         self.gou_add_cart_btn = []
+#########gourment part###########
+        
+    def gourmet(self):
+        
+        #create gourment frame
+        self.regular_frame.grid_forget()
+        ##self.gourment_frame.grid(row = 0, columnspan = 3)        
+        # self.canvas.create_window((0, 30), window=self.gourment_frame)
+        self.gourmet_frame.grid(row = 1, columnspan = 3)
+        # self.regular_frame.grid_propagate(0)
+        gou_num_cols = 3
+        gou_num_rows = 6
+        
         i = 0
         #create gourment pizza images list
         # gou_images = []
-        for r in self.gourment_img:
+        for r in self.gourmet_img:
             gou_images.append(PhotoImage(file = r))
         #divide the whole big gourment frame into 6*3 small product frames, with the arrangment of the first column is used to display images,
         #the second is used to display labels(names, contents) and add-to-cart button, the third column is totally empty.
@@ -199,7 +204,7 @@ class Main:
         for c in range(gou_num_cols):
             if c == 0:
                 for r in range(gou_num_rows):
-                    self.gou_product_img_frame.append(Frame(self.gourment_frame, bg="linen", width = 300, height = 200))
+                    self.gou_product_img_frame.append(Frame(self.gourmet_frame, bg="linen", width = 300, height = 200))
                     self.gou_product_img_frame[r].grid(row = r, column = c)
                     self.gou_product_img_frame[r].grid_propagate(0) 
             
@@ -207,7 +212,7 @@ class Main:
             # if c == 0: #At first column, I'm planning to add images, with width and height setted. 
             elif c == 1:
                 for r in range(gou_num_rows):
-                    self.gou_product_text_frame.append(Frame(self.gourment_frame, bg="linen", width = 441, height = 200))
+                    self.gou_product_text_frame.append(Frame(self.gourmet_frame, bg="linen", width = 441, height = 200))
                     self.gou_product_text_frame[r].grid(row = r, column = c)
                     self.gou_product_text_frame[r].grid_propagate(0)
                 
@@ -223,38 +228,83 @@ class Main:
             frame_count += 1
 
         #the names,images, add-to-cart buttons of each gourment pizza in each gourment product frame
-        for i in range(len(self.gourment_name)):
+        for i in range(len(self.gourmet_name)):
             #print(i, len(self.product_frame), regular_name,images[i])
             
-            self.gou_img_label.append(Label(self.gou_product_img_frame[i], image=gou_images[i], padx = 50))
+            self.gou_img_label.append(Label(self.gou_product_img_frame[i], image=gou_images[i], padx = 50, bg="linen"))
             self.gou_img_label[i].grid(row = 0, column = 0)
 
-            self.gou_name_label.append(Label(self.gou_product_text_frame[i], text = self.gourment_name[i], font=("Conmic Sans MS","20","bold"), fg = "grey"))
+            self.gou_name_label.append(Label(self.gou_product_text_frame[i], text = self.gourmet_name[i], font=("Conmic Sans MS","20","bold"), fg = "dimgray"))
             self.gou_name_label[i].grid(row = 0, column = 1, sticky = W, pady = 0, padx = 25)
             
-            self.gou_content_label.append(Label(self.gou_product_text_frame[i], text = self.gourment_content[i], wraplength = 400, justify = 'left'))
+            self.gou_content_label.append(Label(self.gou_product_text_frame[i], text = self.gourmet_content[i], wraplength = 400, justify = 'left'))
             self.gou_content_label[i].grid(row = 1, column = 1, sticky = N+S+E+W, pady = 0, padx = 25)
 
             self.gou_price_label.append(Label(self.gou_product_text_frame[i], text = "$17", font=("Conmic Sans MS","20","bold"), fg = "red", bg="linen"))
-            self.gou_price_label[i].grid(row = 2, column = 1, sticky = W, pady = 20, padx = 40)
+            self.gou_price_label[i].grid(row = 2, column = 1, sticky = W, pady = 15, padx = 40)
 
-            self.gou_reduce_num_btn.append(Button(self.gou_product_text_frame[i], text = "-"))
-            self.gou_reduce_num_btn[i].grid(row = 2, column = 1, sticky = E, pady = 20, padx = 100)
+            self.gou_reduce_num_btn.append(Button(self.gou_product_text_frame[i], text = "-", command =lambda x=i: self.gou_ReduceNumber(x)))
+            self.gou_reduce_num_btn[i].grid(row = 2, column = 1, sticky = E, pady = 15, padx = 100)
 
-            self.gou_num_label.append(Label(self.gou_product_text_frame[i], text = "0"))
-            self.gou_num_label[i].grid(row = 2, column = 1, sticky = E, pady = 20, padx = 68)
+            self.gou_num_label.append(Label(self.gou_product_text_frame[i], text = self.GouNum[i]))
+            self.gou_num_label[i].grid(row = 2, column = 1, sticky = E, pady = 15, padx = 68)
             
-            self.gou_add_num_btn.append(Button(self.gou_product_text_frame[i], text = "+"))
-            self.gou_add_num_btn[i].grid(row = 2, column = 1, sticky = E, pady = 20, padx = 40)   
+            self.gou_add_num_btn.append(Button(self.gou_product_text_frame[i], text = "+", command =lambda x=i: self.gou_AddNumber(x)))
+            self.gou_add_num_btn[i].grid(row = 2, column = 1, sticky = E, pady = 15, padx = 40)   
             
             self.gou_add_cart_btn.append(Button(self.gou_product_text_frame[i], text = "ADD TO CART", fg = "red"))
-            self.gou_add_cart_btn[i].grid(row = 3, column = 1, sticky = S, pady = 6, padx = 0)
+            self.gou_add_cart_btn[i].grid(row = 3, column = 1, sticky = S, pady =0, padx = 0)
             # i += 1
+            
+################################
+     #create list of the number shown in the number counter, with the initial numbers 0.
+    def re_AddNumber(self,x):         
+        re_index = self.re_add_num_btn.index(self.re_add_num_btn[x]) #find the index of the current button that user clicked in the cutton list
+        self.ReNum[re_index] += 1
+        if self.ReNum[re_index] >= 0 and self.ReNum[re_index] <=5:
+            
+            self.re_num_label[re_index].configure(text = self.ReNum[re_index])
 
-        
-        
-        
+        elif self.ReNum[re_index] >5: #set maximum number that each user can order for each pizza
+            self.ReNum[re_index] = 5
+            
+            self.re_num_label[re_index].configure(text = self.ReNum[re_index])
 
+    def re_ReduceNumber(self,x):        
+        re_index = self.re_reduce_num_btn.index(self.re_reduce_num_btn[x])
+        self.ReNum[re_index] -= 1
+        if self.ReNum[re_index] >= 0 and self.ReNum[re_index] <=5:
+            
+            self.re_num_label[x].configure(text = self.ReNum[re_index])                 
+        
+        elif self.ReNum[re_index] < 0:
+            self.ReNum[re_index] = 0
+            
+            self.re_num_label[re_index].configure(text = self.ReNum[re_index])        
+################################       
+    def gou_AddNumber(self,x):         
+        gou_index = self.gou_add_num_btn.index(self.gou_add_num_btn[x]) #find the index of the current button that user clicked in the cutton list
+        self.GouNum[gou_index] += 1
+        if self.GouNum[gou_index] >= 0 and self.GouNum[gou_index] <=5:
+            
+            self.gou_num_label[gou_index].configure(text = self.GouNum[gou_index])
+
+        elif self.GouNum[gou_index] >5: #set maximum number that each user can order for each pizza
+            self.GouNum[gou_index] = 5
+            
+            self.gou_num_label[gou_index].configure(text = self.GouNum[gou_index])
+
+    def gou_ReduceNumber(self,x):        
+        gou_index = self.gou_reduce_num_btn.index(self.gou_reduce_num_btn[x])
+        self.GouNum[gou_index] -= 1
+        if self.GouNum[gou_index] >= 0 and self.GouNum[gou_index] <=5:
+            
+            self.gou_num_label[x].configure(text = self.GouNum[gou_index])                 
+        
+        elif self.GouNum[gou_index] < 0:
+            self.GouNum[gou_index] = 0
+            
+            self.gou_num_label[gou_index].configure(text = self.GouNum[gou_index])
 
 #main
 if __name__ == '__main__':
